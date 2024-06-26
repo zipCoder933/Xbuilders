2b424894e1074e17ab8fb3ce43e36f927a661ee9
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2024-06-26 17:41:32 -0600

    updated player run speed to match xb2 speed

--- src/main/java/com/xbuilders/engine/player/UserControlledPlayer.java
+++ src/main/java/com/xbuilders/engine/player/UserControlledPlayer.java
@@ -41,7 +41,7 @@ public class UserControlledPlayer extends Player {
 
     final static float FLY_SPEED = 12f;
     final float DEFAULT_SPEED = 12f;
-    final float RUN_SPEED = DEFAULT_SPEED * 5;//XB2: runSpeed = 7.0f;
+    final float RUN_SPEED = DEFAULT_SPEED * 2.5f;//XB2 runSpeed = 12f * 2.5f
 
     Matrix4f projection;
     Matrix4f view;
