e187b6cb3ac7d1f3579eaf794cc05e12198d826d
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2024-07-04 13:00:07 -0600

    Update UIResources.java

--- src/main/java/com/xbuilders/engine/ui/UIResources.java
+++ src/main/java/com/xbuilders/engine/ui/UIResources.java
@@ -25,18 +25,18 @@ public class UIResources {
     public NkUserFont font_24, font_22, font_18, font_12, font_10, font_8, font_6;
     ByteBuffer fontBuffer;
 
-    public UIResources(NKWindow window, NkContext ctx, boolean largerUI) throws IOException {
+    public UIResources(NKWindow window, NkContext ctx, boolean largerFonts) throws IOException {
         String path = ResourceUtils.RESOURCE_DIR + "\\fonts\\Press_Start_2P\\PressStart2P-Regular.ttf";
         System.out.println("Font: " + path);
         fontBuffer = ioResourceToByteBuffer(path, 512 * 1024);
 
-        font_24 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 26 : 24);
-        font_22 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 24 : 22);
-        font_18 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 20 : 18);
-        font_12 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 14 : 12);
-        font_10 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 12 : 10);
-        font_8 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 10 : 8);
-        font_6 = window.TTF_assignToNewTexture(fontBuffer, largerUI ? 10 : 6);
+        font_24 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 26 : 24);
+        font_22 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 24 : 22);
+        font_18 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 20 : 18);
+        font_12 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 14 : 12);
+        font_10 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 12 : 10);
+        font_8 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 10 : 8);
+        font_6 = window.TTF_assignToNewTexture(fontBuffer, largerFonts ? 10 : 6);
 
         //VERY IMPORTANT: a font must be specified before anything can be drawn
         nk_style_set_font(ctx, font_8);
