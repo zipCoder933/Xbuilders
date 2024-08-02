f80f634e21bcd7c0cd01d8be5b21d4a26753ad0d
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2024-08-02 11:26:58 -0600

    updated jar

--- src/main/java/com/xbuilders/game/Main.java
+++ src/main/java/com/xbuilders/game/Main.java
@@ -34,7 +34,7 @@ import java.util.logging.Logger;
 
 public class Main extends NKWindow {
 
-    public static double gameVersion = 1.3;
+    public static double gameVersion = 1.4;
     public static boolean loadWorldOnStartup = false;
 
     public static void printlnDev(String message) {
