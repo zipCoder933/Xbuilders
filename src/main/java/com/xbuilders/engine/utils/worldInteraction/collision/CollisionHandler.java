58d06624614e1701d39a463d7c9e055e90bc525b
zipCoder933 <sam.p.w524@gmail.com>
2025-01-03 13:13:01 -0700

    Update CollisionHandler.java

--- src/main/java/com/xbuilders/engine/utils/worldInteraction/collision/CollisionHandler.java
+++ src/main/java/com/xbuilders/engine/utils/worldInteraction/collision/CollisionHandler.java
@@ -76,63 +76,13 @@ public class CollisionHandler {
             setFrozen = false;
             exploredChunks.clear();
 
-            // Y goes down so that we can sort blocks from top (ceiling) to bottom
-            for (int y = (int) (myBox.box.max.y + BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); y >= myBox.box.min.y
-                    - BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; y--) {
-                // for (int y = (int) (myBox.box.minPoint.y -
-                // BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); y <= myBox.box.maxPoint.y +
-                // BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; y++) {
-                for (int x = (int) (myBox.box.min.x - BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); x <= myBox.box.max.x
-                        + BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; x++) {
-                    for (int z = (int) (myBox.box.min.z - BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); z <= myBox.box.max.z
-                            + BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; z++) {
-                        wcc.set(x, y, z);
-                        chunk = chunks.getChunk(wcc.chunk);
-                        if (chunk != null) {
-                            exploredChunks.add(chunk);
-                            // if (Main.specialMode1) {
-                            b = Registrys.blocks.getBlock(chunk.data.getBlock(
-                                    wcc.chunkVoxel.x,
-                                    wcc.chunkVoxel.y,
-                                    wcc.chunkVoxel.z));
-                            if (b != null) {
-                                if (b.solid) {
-                                    // if (Main.specialMode2) {
-                                    // TODO: chunk.getBlockData() is collision-handler memory culprit!!!
-                                    // Its ALL in the hashmap...
-                                    d = chunk.data.getBlockData(
-                                            wcc.chunkVoxel.x,
-                                            wcc.chunkVoxel.y,
-                                            wcc.chunkVoxel.z);
-                                    // }
-                                    BlockType type = Registrys.blocks.getBlockType(b.renderType);
-                                    try {
-                                        if (type != null) {
-                                            type.getCollisionBoxes((aabb) -> {
-                                                processBox(aabb, b, false);
-                                            }, collisionBox, b, d, x, y, z);
-                                        }
-                                    } catch (Exception e) {
-                                        ErrorHandler.log(e);
-                                    }
-                                }
-                            }
-                            // }
-                        }
-                    }
-                }
-            }
+            processBlockCollisions();
 
             for (Chunk chunk : exploredChunks) {
                 for (int i = 0; i < chunk.entities.list.size(); i++) {
                     compareEntityAABB(projection, view, chunk.entities.list.get(i).aabb);
                 }
             }
-//            if (playerList != null) {
-//                for (int i = 0; i < playerList.size(); i++) {
-//                    compareEntityAABB(projection, view, playerList.get(i).aabb);
-//                }
-//            }
             // Comparison against user controlled player (all entity and player boxes are
             // skipped if they match themselves)
             if (userControlledPlayerAABB != null) compareEntityAABB(projection, view, userControlledPlayerAABB);
@@ -141,36 +91,103 @@ public class CollisionHandler {
         }
     }
 
+    private void processBlockCollisions() {
+        // Y goes down so that we can sort blocks from top (ceiling) to bottom
+        for (int y = (int) (myBox.box.max.y + BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); y >= myBox.box.min.y
+                - BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; y--) {
+            for (int x = (int) (myBox.box.min.x - BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); x <= myBox.box.max.x
+                    + BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; x++) {
+                for (int z = (int) (myBox.box.min.z - BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS); z <= myBox.box.max.z
+                        + BLOCK_COLLISION_CANDIDATE_CHECK_RADIUS; z++) {
+                    wcc.set(x, y, z);
+                    chunk = chunks.getChunk(wcc.chunk);
+                    if (chunk != null) {
+                        exploredChunks.add(chunk);
 
-    long lastIllegalFloorCollisionTime = 0;
+                        b = Registrys.blocks.getBlock(chunk.data.getBlock(
+                                wcc.chunkVoxel.x,
+                                wcc.chunkVoxel.y,
+                                wcc.chunkVoxel.z));
+                        if (b != null) {
+                            if (b.solid) {
+                                // if (Main.specialMode2) {
+                                // TODO: chunk.getBlockData() is collision-handler memory culprit!!!
+                                // Its ALL in the hashmap...
+                                d = chunk.data.getBlockData(
+                                        wcc.chunkVoxel.x,
+                                        wcc.chunkVoxel.y,
+                                        wcc.chunkVoxel.z);
+                                // }
+                                BlockType type = Registrys.blocks.getBlockType(b.renderType);
+                                try {
+                                    if (type != null) {
+                                        type.getCollisionBoxes((aabb) -> {
+                                            processBox(aabb, b, false);
+                                        }, collisionBox, b, d, x, y, z);
+                                    }
+                                } catch (Exception e) {
+                                    ErrorHandler.log(e);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private boolean calculateCollisionData(AABB box, Block block, boolean isEntity) {
+        collisionData.calculateCollision(box, myBox.box, isEntity);
+        if (isEntity) {
+            collisionData.penPerAxes.mul(0.8f);
+        }
+        if (driver.velocity.y >= driver.maxFallSpeed * 0.99f) {//Hard collision
+            if (collisionData.collisionNormal.y == -1) { //If we are hitting the ground hard, we still want to collide with it
+                handleFloorCollision(box, block);
+            }
+        } else if (Math.abs(collisionData.penPerAxes.x) > 0.6f ||
+                Math.abs(collisionData.penPerAxes.z) > 0.6f ||
+                Math.abs(collisionData.penPerAxes.y) > 0.6f) {
+            driver.velocity.y = 0;
+            setFrozen = true;
+            return false;
+        }
+        return true;
+    }
+
+//    private void processBoxXZ(AABB box, Block block, boolean isEntity) {
+//        if (box.intersects(myBox.box) && calculateCollisionData(box, block, isEntity)) {
+//            if (collisionData.collisionNormal.x != 0) {
+//                if (myBox.box.max.y - box.min.y < driver.STEP_HEIGHT) {
+//                    myBox.box.setY(myBox.box.min.y - Math.abs(collisionData.penPerAxes.x));
+//                } else {
+//                    myBox.box.setX(myBox.box.min.x + (collisionData.penPerAxes.x));
+//                }
+//            } else if (collisionData.collisionNormal.z != 0) {
+//                if (myBox.box.max.y - box.min.y < driver.STEP_HEIGHT) {
+//                    myBox.box.setY(myBox.box.min.y - Math.abs(collisionData.penPerAxes.z));
+//                } else {
+//                    myBox.box.setZ(myBox.box.min.z + (collisionData.penPerAxes.z));
+//                }
+//            }
+//        }
+//    }
+//
+//    private void processBoxY(AABB box, Block block, boolean isEntity) {
+//        if (box.intersects(myBox.box) && calculateCollisionData(box, block, isEntity)) {
+//            //Floor collision
+//            if (collisionData.collisionNormal.y == -1) {
+//                handleFloorCollision(box, block);
+//            } else if (collisionData.collisionNormal.y == 1 && box.min.y < myBox.box.min.y) { //Ceiling collision
+//                driver.velocity.y = 0;
+//                myBox.box.setY(myBox.box.min.y + collisionData.penPerAxes.y);
+//            }
+//        }
+//    }
 
     private void processBox(AABB box, Block block, boolean isEntity) {
-        // if (stepBox.intersects(box)) {
-        // stepWillHitCeiling = true;
-        // System.out.println("STEP HIT CEILING " + System.currentTimeMillis());
-        // }
-
-        if (box.intersects(myBox.box)) {
-
-            collisionData.calculateCollision(box, myBox.box, isEntity);
-
-            if (isEntity) {
-                collisionData.penPerAxes.mul(0.8f);
-            }
-
-            if (driver.velocity.y >= driver.maxFallSpeed * 0.99f) {//Hard collision
-//                System.out.println("HARD COLLISION!: " + collisionData.penPerAxes);
-                if (collisionData.collisionNormal.y == -1) { //If we are hitting the ground hard, we still want to collide with it
-                    handleFloorCollision(box, block);
-                }
-            } else if (Math.abs(collisionData.penPerAxes.x) > 0.6f ||
-                    Math.abs(collisionData.penPerAxes.z) > 0.6f ||
-                    Math.abs(collisionData.penPerAxes.y) > 0.6f) {
-                driver.velocity.y = 0;
-                setFrozen = true;
-                return;
-            }
-
+        if (box.intersects(myBox.box) && calculateCollisionData(box, block, isEntity)) {
             if (collisionData.collisionNormal.x != 0) {
                 if (myBox.box.max.y - box.min.y < driver.STEP_HEIGHT) {
                     myBox.box.setY(myBox.box.min.y - Math.abs(collisionData.penPerAxes.x));
@@ -195,8 +212,6 @@ public class CollisionHandler {
                 driver.velocity.y = 0;
                 myBox.box.setY(myBox.box.min.y + collisionData.penPerAxes.y);
             }
-
-
         }
     }
 
