ad6089ce9d9adca8c5e60cb7c9d1ae0e1117ba83
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-01-03 20:46:05 -0700

    updated jarfile

--- src/main/java/com/xbuilders/Main.java
+++ src/main/java/com/xbuilders/Main.java
@@ -3,7 +3,7 @@ package com.xbuilders;
 import com.xbuilders.engine.MainWindow;
 
 public class Main {
-    public static final String GAME_VERSION = "1.2.0";
+    public static final String GAME_VERSION = "1.3.0";
     public static void main(String[] args) {
         new MainWindow(args);
     }
