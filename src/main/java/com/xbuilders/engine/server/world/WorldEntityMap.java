7bcc03e38976921306cba78cbeb1984b043f456c
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-03-07 20:42:35 -0700

    fixed bug with remove all entities

--- src/main/java/com/xbuilders/engine/server/world/WorldEntityMap.java
+++ src/main/java/com/xbuilders/engine/server/world/WorldEntityMap.java
@@ -7,16 +7,25 @@ import java.util.HashMap;
 
 public class WorldEntityMap extends HashMap<Long, Entity> {
     public void removeAllEntitiesFromChunk(Chunk chunk) {
-        for (Entity e : new HashMap<>(this).values()) {
-            if (e.chunkPosition.chunk.equals(chunk.position)) {
-                remove(e.getUniqueIdentifier());
+        try {
+            for (Entity e : new HashMap<>(this).values()) {
+                if (e.chunkPosition.chunk.equals(chunk.position)) {
+                    remove(e.getUniqueIdentifier());
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
     public void addAllEntitiesFromChunk(Chunk chunk) {
-        for (Entity e : chunk.entities.list) {
-            put(e.getUniqueIdentifier(), e);
+        try {
+            for (Entity e : chunk.entities.list) {
+                put(e.getUniqueIdentifier(), e);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
