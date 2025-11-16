7ccbe32d4faf908c56c030c39e96e203257af4dc
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-11-16 10:19:47 -0700

    revert new blocks added

--- src/main/java/com/xbuilders/content/vanilla/blocks/type/FloorItemRenderer.java
+++ src/main/java/com/xbuilders/content/vanilla/blocks/type/FloorItemRenderer.java
@@ -4,7 +4,6 @@
  */
 package com.xbuilders.content.vanilla.blocks.type;
 
-import com.xbuilders.content.vanilla.blocks.RenderType;
 import com.xbuilders.engine.client.Client;
 import com.xbuilders.engine.server.block.Block;
 import com.xbuilders.engine.server.block.construction.BlockType;
@@ -21,7 +20,6 @@ import java.io.IOException;
  * @author zipCoder933
  */
 public class FloorItemRenderer extends BlockType {
-
     BlockModel floor0, floor1, floor2, floor3;
 
 
@@ -38,23 +36,9 @@ public class FloorItemRenderer extends BlockType {
         floor1 = BlockModelLoader.load(resourceLoader.getResourceAsStream("/assets/xbuilders/models/block/floor/floor1.blockType"), renderSide_subBlock);
         floor2 = BlockModelLoader.load(resourceLoader.getResourceAsStream("/assets/xbuilders/models/block/floor/floor2.blockType"), renderSide_subBlock);
         floor3 = BlockModelLoader.load(resourceLoader.getResourceAsStream("/assets/xbuilders/models/block/floor/floor3.blockType"), renderSide_subBlock);
-
         initializationCallback = (b) -> {
             b.initialBlockData = (existingData, player) -> {
-                BlockData bd = player.camera.simplifiedPanTiltAsBlockData(new BlockData(2));
-                Block hitBlock = Client.world.getBlock(player.camera.cursorRay.getHitPos());
-
-                if (player.camera.cursorRay.getHitNormal().y == -1
-                        || !hitBlock.solid
-                        || hitBlock.type == RenderType.FLOOR
-                        || hitBlock.type == RenderType.WALL_ITEM
-                        || hitBlock.type == RenderType.SPRITE
-                        || hitBlock.type == RenderType.FENCE
-                        || hitBlock.type == RenderType.FENCE_GATE
-                        || hitBlock.type == RenderType.DOOR_HALF) {
-                    bd.set(1, (byte) 1);//Make this a floor
-                }
-                return bd;
+                return player.camera.simplifiedPanTiltAsBlockData(new BlockData(2));
             };
 
             b.opaque = false;
@@ -65,7 +49,6 @@ public class FloorItemRenderer extends BlockType {
 
     @Override
     public boolean constructBlock(VertexSet buffers, Block block, BlockData data, Block[] neighbors, BlockData[] neighborData, byte[] light, Chunk chunk, int chunkX, int chunkY, int chunkZ, boolean isUsingGreedyMesher) {
-
         if (data == null || data.size() < 1 || data.get(0) == 3) {
             floor0.render(buffers, block, neighbors, light, chunkX, chunkY, chunkZ);
         } else if (data.get(0) == 0) {
@@ -75,24 +58,21 @@ public class FloorItemRenderer extends BlockType {
         } else {
             floor3.render(buffers, block, neighbors, light, chunkX, chunkY, chunkZ);
         }
-
         return false;
     }
 
-
-    private final float ONE_SIXTEENTH = 0.0625f;
+    private final float sixteenthConstant = 0.0625f;
 
     @Override
     public void getCollisionBoxes(BoxConsumer consumer, AABB box, Block block, BlockData data, int x, int y, int z) {
-
-        box.setPosAndSize(x, y + (ONE_SIXTEENTH * 15), z, 1, ONE_SIXTEENTH * 1, 1);
+        box.setPosAndSize(x, y + (sixteenthConstant * 15), z, 1, sixteenthConstant * 1, 1);
         consumer.accept(box);
-
     }
 
     @Override
     public void getCursorBoxes(BoxConsumer consumer, AABB box, Block block, BlockData data, int x, int y, int z) {
-        getCollisionBoxes(consumer, box, block, data, x, y, z);
+        box.setPosAndSize(x, y + (sixteenthConstant * 15), z, 1, sixteenthConstant * 1, 1);
+        consumer.accept(box);
     }
 
 }
